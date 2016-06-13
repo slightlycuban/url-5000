322b66951350b55af0cad25e1316de8ea09411a1
Michael Tracy <slightlycuban@gmail.com>
2016-06-13 06:51:29 -0400

    Use handlebars and vision to render some views

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@
 module.exports = [{
   path: '/',
   method: 'GET',
-  handler: (request, reply) => {
-    reply('url 5000: your operator for the internet');
+  handler: {
+    view: 'index'
   }
 }];
--- server.js
+++ server.js
@@ -12,13 +12,25 @@ const server = new Hapi.Server({
 server.connection({ port: 3000 });
 
 server.register([{
-  register: require('hapi-router'),
-  options: { routes: 'routes/*.js' }
-}, {
   register: require('good'),
   options: logConfig
+}, {
+  register: require('vision')
+}, {
+  register: require('hapi-router'),
+  options: { routes: 'routes/*.js' }
 }], (err) => {
   if (err) { throw err; }
+
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: './templates',
+    layout: true,
+    layoutPath: './templates/layouts'
+  });
 
 	server.start(() => {
 		console.log('Server running at:', server.info.uri);
